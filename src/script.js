09aac1b5defadaa727f33e7a819a0c7fa26940fd
ArianeDubois <36155355+ArianeDubois@users.noreply.github.com>
2022-04-16 02:15:46 +0200

    push

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 import gsap, { random } from 'gsap'
 
-var scene = document.querySelector('a-scene');
+var scene = document.querySelector('a-entity');
 
 if (scene.hasLoaded) {
     run();
